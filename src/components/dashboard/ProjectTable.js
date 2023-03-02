5e683d509e94bb67e2e757ec3803af01200b15e2
angelisM <t8200001@aueb.gr>
2023-03-02 19:16:55 +0200

    customizing homepage

---
+++ src/components/dashboard/ProjectTable.js
@@ -0,0 +1,129 @@
+import {Card, CardBody, CardTitle, CardSubtitle, Table, Badge} from "reactstrap";
+import user1 from "../../assets/images/users/user1.jpg";
+import user2 from "../../assets/images/users/user2.jpg";
+import user3 from "../../assets/images/users/user3.jpg";
+import user4 from "../../assets/images/users/user4.jpg";
+import user5 from "../../assets/images/users/user5.jpg";
+
+const tableData = [
+  {
+    avatar: user1,
+    name: "Hanna Gover",
+    email: "hgover@gmail.com",
+    project: "Flexy React",
+    status: "pending",
+    weeks: "35",
+    budget: "95K",
+    date: "6 minute ago",
+  },
+  {
+    avatar: user2,
+    name: "Hanna Gover",
+    email: "hgover@gmail.com",
+    project: "Lading pro React",
+    status: "done",
+    weeks: "35",
+    budget: "95K",
+    date: "6 minute ago",
+
+  },
+  {
+    avatar: user3,
+    name: "Hanna Gover",
+    email: "hgover@gmail.com",
+    project: "Elite React",
+    status: "holt",
+    weeks: "35",
+    budget: "95K",
+    date: "6 minute ago",
+
+  },
+  {
+    avatar: user4,
+    name: "Hanna Gover",
+    email: "hgover@gmail.com",
+    project: "Flexy React",
+    status: "pending",
+    weeks: "35",
+    budget: "95K",
+    date: "6 minute ago",
+
+  },
+  {
+    avatar: user5,
+    name: "Hanna Gover",
+    email: "hgover@gmail.com",
+    project: "Ample React",
+    status: "done",
+    weeks: "35",
+    budget: "95K",
+    date: "6 minute ago",
+
+  },
+];
+
+const ProjectTables = () => {
+  return (
+    <div>
+      <Card>
+        <CardBody>
+          <CardTitle tag="h2">Family Messages</CardTitle>
+          <CardSubtitle className="mb-2 text-muted" tag="h4">
+            Will be personalized if user logs in
+          </CardSubtitle>
+
+          <Table className="no-wrap mt-3 align-middle" responsive borderless>
+            <thead>
+              <tr>
+                <th><h3>User</h3></th>
+                <th><h3>Status</h3></th>
+                <th><h3>Messages</h3></th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableData.map((tdata, index) => (
+                <tr key={index} className="border-top">
+                  <td>
+                    <div className="d-flex align-items-center p-2">
+                      <img
+                        src={tdata.avatar}
+                        className="rounded-circle"
+                        alt="avatar"
+                        width="45"
+                        height="45"
+                      />
+                      <div className="ms-3">
+                        <h6 className="mb-0">{tdata.name}</h6>
+                        <span className="text-muted">{tdata.email}</span>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+
+                    {tdata.status === "pending" ? (
+                        <Badge color="danger" className="ms-3">
+                          Urgent
+                        </Badge>
+                    ) : tdata.status === "holt" ? (
+                        <Badge color="warning" className="ms-3">
+                          Warning
+                        </Badge>
+                    ) : (
+                        <Badge color="success" className="ms-3">
+                          Ok
+                        </Badge>
+                    )}
+                  </td>
+                  <td>{tdata.project}</td>
+                  <td>{tdata.date}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default ProjectTables;
---
+++ src/views/Starter.js
@@ -0,0 +1,146 @@
+import { Col, Row } from "reactstrap";
+import SalesChart from "../components/dashboard/SalesChart";
+import Feeds from "../components/dashboard/Feeds";
+import ProjectTables from "../components/dashboard/ProjectTable";
+import TopCards from "../components/dashboard/TopCards";
+import Blog from "../components/dashboard/Blog";
+import bg1 from "../assets/images/bg/bg1.jpg";
+import bg2 from "../assets/images/bg/bg2.jpg";
+import bg3 from "../assets/images/bg/bg3.jpg";
+import bg4 from "../assets/images/bg/bg4.jpg";
+import Clock from "../components/dashboard/Clock";
+import React, {useEffect, useState} from "react";
+
+const BlogData = [
+  {
+    image: bg1,
+    title: "Pill#1",
+    subtitle: "2 comments, 1 Like",
+    description: "Useful info for the effects and ",
+    btnbg: "primary",
+  },
+  {
+    image: bg2,
+    title: "Pill#2",
+    subtitle: "2 comments, 1 Like",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    btnbg: "primary",
+  },
+  {
+    image: bg3,
+    title: "Pill#3",
+    subtitle: "2 comments, 1 Like",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    btnbg: "primary",
+  },
+  {
+    image: bg4,
+    title: "Pill#4",
+    subtitle: "2 comments, 1 Like",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    btnbg: "primary",
+  },
+];
+
+function Starter(){
+
+  const [dateState, setDateState] = useState(new Date());
+  useEffect(() => {
+    setInterval(() => setDateState(new Date()), 30000);
+  }, []);
+
+  let date = dateState.toLocaleDateString('en-GB', {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
+  });
+
+  let time = dateState.toLocaleString('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  });
+
+  return (
+    <div>
+      {/***Clock***/}
+      <Row>
+        <Clock/>
+      </Row>
+      {/***Top Cards***/}
+      <Row>
+        <h3>This is where the clock component will be called</h3>
+        <Col sm="6" lg="3">
+          <TopCards
+            bg="bg-light-success text-success"
+            title="Profit"
+            subtitle="Yearly Earning"
+            earning="$21k"
+            icon="bi bi-clock"
+          />
+        </Col>
+        <Col sm="6" lg="3">
+          <TopCards
+            bg="bg-light-danger text-danger"
+            title="Refunds"
+            subtitle="Refund given"
+            earning="$1k"
+            icon="bi bi-calendar"
+          />
+        </Col>
+        <Col sm="6" lg="3">
+          <TopCards
+            bg="bg-light-warning text-warning"
+            title="New Project"
+            subtitle="Yearly Project"
+            earning="456"
+            icon="bi bi-basket3"
+          />
+        </Col>
+        <Col sm="6" lg="3">
+          <TopCards
+            bg="bg-light-info text-into"
+            title="Sales"
+            subtitle="Weekly Sales"
+            earning="210"
+            icon="bi bi-bag"
+          />
+        </Col>
+      </Row>
+      {/***Sales & Feed***/}
+      <Row>
+        {/*<Col sm="6" lg="6" xl="7" xxl="8">
+          <SalesChart />
+        </Col>*/}
+        <Col sm="6" lg="6" xl="5" xxl="7">
+          <Feeds />
+        </Col>
+      </Row>
+      {/***Table ***/}
+      <Row>
+        <Col lg="8">
+          <ProjectTables />
+        </Col>
+      </Row>
+      {/***Blog Cards***/}
+      <Row>
+        {BlogData.map((blg, index) => (
+          <Col sm="6" lg="6" xl="3" key={index}>
+            <Blog
+              image={blg.image}
+              title={blg.title}
+              subtitle={blg.subtitle}
+              text={blg.description}
+              color={blg.btnbg}
+            />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
+export default Starter;
